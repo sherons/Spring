27d8f98372c0676be60ec0c7fa9b1709d4f21cca
Meenu <Meenu@Meenu>
2017-03-18 14:14:53 -0600

    Commit on March 18, 2017

---
+++ src/main/java/com/webapp/springmvc/LoginController.java
@@ -0,0 +1,36 @@
+package com.webapp.springmvc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.webapp.jee.LoginService;
+
+
+@Controller
+public class LoginController {
+	
+	@Autowired
+	LoginService service;
+
+	@RequestMapping(value="/login", method=RequestMethod.GET)
+	public String showLoginPage(){
+		return "login";
+	}
+
+	@RequestMapping(value="/login", method=RequestMethod.POST)
+	public String handleLoginRequest(ModelMap model, @RequestParam String name, @RequestParam String password){
+		if(!service.validateUser(name, password)){
+			model.put("errorMessage", "Invalid Credentials!");
+			return "login";
+		}
+		
+		model.put("attname", name);
+		model.put("attpass", password);
+		return "welcome";
+	}
+	
+}
